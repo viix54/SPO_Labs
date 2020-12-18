82087905c728c67ad416149fd713bf9b222ad53e
viix54 <76229411+viix54@users.noreply.github.com>
2020-12-19 00:24:16 +0300

    SPO_Labs
    Lab 5 and Lab 6

---
+++ Laba_5_SPO — копия/Laba_5_SPO.cpp
@@ -0,0 +1,70 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <windows.h>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int mas [10];
+
+int thread01()
+{
+	srand(time(NULL));
+	HANDLE hMutex01 = OpenMutex(SYNCHRONIZE, FALSE, "MyMutex");
+	if (hMutex01 == NULL)
+		cout << "Open mutex01 failed" << GetLastError() << endl;
+	WaitForSingleObject(hMutex01, INFINITE);
+	for (int i = 0; i < 10; i++)
+	{
+		mas[i] = rand();
+		cout << mas[i] << " ";
+	};
+	cout << endl;
+	ReleaseMutex(hMutex01);
+	CloseHandle(hMutex01);
+	return 0;
+}
+
+int thread02()
+{
+	HANDLE hMutex02 = OpenMutex(SYNCHRONIZE, FALSE, "MyMutex");
+	Sleep(1);
+	if (hMutex02 == NULL)
+		cout << "Open mutex02 failed" << GetLastError() << endl;
+	WaitForSingleObject(hMutex02, INFINITE);
+	for (int i = 0; i < 10; i++)
+	{
+		if (mas[i] % 2 == 0)
+		{
+			mas[i] = 0;
+		}
+		cout << mas[i] << " ";
+	}
+	cout << endl;
+	ReleaseMutex(hMutex02);
+	CloseHandle(hMutex02);
+	return 0;
+}
+
+int main()
+{
+	HANDLE hMutex = CreateMutex(NULL, FALSE, "MyMutex");
+	if (hMutex == NULL)
+		cout << "Create mutex failed" << GetLastError() << endl;
+
+	HANDLE hTh01 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread01, NULL, 0, NULL);
+	if (hTh01 == NULL) return GetLastError();
+
+	HANDLE hTh02 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread02, NULL, 0, NULL);
+	if (hTh02 == NULL)return GetLastError();
+
+	cin.get();
+
+	CloseHandle(hMutex);
+
+	CloseHandle(hTh01);
+	CloseHandle(hTh02);
+	system("pause");
+	return 0;
+}
+
---
+++ Лаба_6_СПО/Лаба_6_СПО.cpp
@@ -0,0 +1,70 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <windows.h>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+HANDLE hSemaphore;
+int n;
+int *mas = new int[n];
+
+int thread01() 
+{
+	HANDLE Semaphore1 = OpenSemaphore(SYNCHRONIZE, FALSE, "MySemaphore");
+	srand(time(NULL));
+	for (int i = 0; i < n; i++)
+	{
+		mas[i] = rand() % 501;
+		cout << mas[i] << " ";
+		Sleep(100);
+	}
+	ReleaseSemaphore(hSemaphore, 1, NULL);
+	cout << endl;
+	CloseHandle(Semaphore1);
+	return 0;
+}
+
+int thread02()
+{
+	Sleep(1000);
+	HANDLE Semaphore2 = OpenSemaphore(SYNCHRONIZE, FALSE, "MySemaphore");
+	int k = 0;
+	WaitForSingleObject(hSemaphore, INFINITE);
+	for (int i = 0; i < n; i++)
+	{
+		if ( mas[i] % 2 == 0 && mas[i] > 0)
+		{
+			k++;
+		}
+	}
+	cout << "Chetnye and Neotricatelnyje: " << k << endl;
+	CloseHandle(Semaphore2);
+	return 0;
+}
+
+
+
+int main()
+{
+	cout << "Enter arrange number:";
+	cin >> n;
+	hSemaphore = CreateSemaphore(NULL, 1, n+1, "MySemaphore");
+	if (hSemaphore == NULL)
+		cout << "Create semaphore failed" << GetLastError() << endl;
+
+	HANDLE hTh01 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread01, NULL, 0, NULL);
+	if (hTh01 == NULL)
+		return GetLastError();
+	HANDLE hTh02 = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)thread02, NULL, 0, NULL);
+	if (hTh02 == NULL) return GetLastError();
+	
+	cin.get();
+	
+	CloseHandle(hSemaphore);
+
+	CloseHandle(hTh01);
+	CloseHandle(hTh02);
+	return 0;
+}
+
+
